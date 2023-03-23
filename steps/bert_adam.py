5942620ad3de1d1269a1fc35ff4b05ac56e17bb0
khazarkhorrami <khazar.khorrami@gmail.com>
2023-03-23 14:27:19 +0200

    correcting a print

--- steps/bert_adam.py
+++ steps/bert_adam.py
@@ -118,7 +118,7 @@ class BertAdam(Optimizer):
             loss = closure()
 
         warned_for_t_total = False
-        print(".............we are inside optimizer adam class step function .............")
+        #print(".............we are inside optimizer adam class step function .............")
         for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None:
