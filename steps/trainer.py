7a99d1ec4bf0ee2441a06ba8d9a42bb50beb7d13
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-04-19 16:43:53 +0300

    masking MSCOCO

--- steps/trainer.py
+++ steps/trainer.py
@@ -424,8 +424,8 @@ class Trainer:
                         #img_feats_list.append(detached_visual_feats[j])
                         img_cls_list.append(visual_cls[j].detach())
                         img_img_id_list.append(img_id)
-                if i>= 300:
-                    break
+                # if i>= 300:
+                #     break
             
             print ('khazar: memory allocated before cat')
             print(torch.cuda.memory_allocated(device=0) / 1024 ** 3)
