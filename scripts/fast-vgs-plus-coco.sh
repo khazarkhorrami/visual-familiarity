0119f590306f83579e91481c92f1cb9fc6a1d26c
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-05-29 10:47:02 +0300

    sub0 for puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -22,7 +22,7 @@ python \
 --val_cross_batch_size 100 \
 --n_epochs 70 \
 --n_print_steps 10 \
---n_val_steps 59 \
+--n_val_steps 40 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
