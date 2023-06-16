308be70b550eaf0fb4f8a08100dc248f160f7c81
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-06-16 23:06:11 +0300

    bs 64 for puhti LS

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -17,12 +17,12 @@ python \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 32 \
+--batch_size 64 \
 --val_batch_size 100 \
 --val_cross_batch_size 100 \
 --n_epochs 30 \
 --n_print_steps 100 \
---n_val_steps 1000 \
+--n_val_steps 9100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -30,12 +30,13 @@ python \
 --vit_checkpoint_key 'teacher' \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 6 \
+--trm_layers 1 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 1.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
---layer_use 7 \
+--encoder_layers 6 \
+--layer_use 4 \
 
