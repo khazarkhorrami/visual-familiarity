0ec2da247fbbb49f33a04fbe391a4fb2c889bb26
khazarkhorrami <khazar.khorrami@gmail.com>
2023-06-16 17:22:08 +0300

    juova

--- scripts/fast-vgs-plus-coco-j.sh
+++ scripts/fast-vgs-plus-coco-j.sh
@@ -21,8 +21,8 @@ python \
 --val_batch_size 16 \
 --val_cross_batch_size 100 \
 --n_epochs 30 \
---n_print_steps 10 \
---n_val_steps 39 \
+--n_print_steps 50 \
+--n_val_steps 2000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -30,12 +30,14 @@ python \
 --vit_checkpoint_key 'teacher' \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 6 \
+--trm_layers 3 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 1.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
---layer_use 7 \
+--encoder_layers 6 \
+--encoder_attention_heads 4 \
+--layer_use 4 \
 
