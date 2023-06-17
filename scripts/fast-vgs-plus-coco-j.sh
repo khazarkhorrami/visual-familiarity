e43deebb18dab8b8b43771941ee21e85796e9e8b
khazarkhorrami <khazar.khorrami@gmail.com>
2023-06-17 13:34:44 +0300

    to test iter libri

--- scripts/fast-vgs-plus-coco-j.sh
+++ scripts/fast-vgs-plus-coco-j.sh
@@ -22,7 +22,7 @@ python \
 --val_cross_batch_size 100 \
 --n_epochs 30 \
 --n_print_steps 50 \
---n_val_steps 2000 \
+--n_val_steps 147100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
