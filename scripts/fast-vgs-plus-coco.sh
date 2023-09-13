51bab365fc49ba2dd7503d761969a7fe965bfda7
khazarkhorrami <khazar.khorrami@gmail.com>
2023-09-13 10:52:33 +0300

    testing init problem

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
 exp_dir="../../exp/"
-libri_fn_root="../../../../datavf/ssl6M_SC/"
+libri_fn_root="../../../../datavf/ssl_COCO/"
 pretrained_root="../../../../hubertAndDINO"
 
 python \
@@ -20,7 +20,7 @@ python \
 --val_cross_batch_size 100 \
 --n_epochs 100 \
 --n_print_steps 500 \
---n_val_steps 5783 \
+--n_val_steps 9178 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
