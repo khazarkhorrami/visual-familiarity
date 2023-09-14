7491abf3309933983951d71eb3612961656c3241
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-09-14 19:48:41 +0300

    vgs+

--- datasets/spokencoco_dataset.py
+++ datasets/spokencoco_dataset.py
@@ -74,7 +74,7 @@ class ImageCaptionDataset(Dataset):
         
         # for masked and blured images:
         if split == "train":
-            self.image_base_path = os.path.join('../../../../datavf/', "coco_pyp/MSCOCO/masked/subset1")
+            self.image_base_path = os.path.join('../../../../datavf/', "coco_pyp/MSCOCO/blured/subset1")
         elif split == "val" or split == "dev":
             self.image_base_path = os.path.join(args.data_root, "coco_pyp/MSCOCO")
         
