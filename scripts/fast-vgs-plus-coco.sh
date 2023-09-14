584f348c9ae32b518778d256c3099c43bf980dc3
khazarkhorrami <khazar.khorrami@gmail.com>
2023-09-14 21:52:25 +0300

    vgs+

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -7,7 +7,6 @@ fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
 exp_dir="../../exp/"
 libri_fn_root="../../../../datavf/ssl6M_root/"
 pretrained_root="../../../../hubertAndDINO"
-twd="../../twd/"
 
 python \
 ../run_spokencoco.py \
@@ -15,14 +14,13 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---trained_weights_dir ${twd} \
 --load_pretrained_vit ${pretrained_root} \
 --batch_size 64 \
 --val_batch_size 64 \
 --val_cross_batch_size 100 \
---n_epochs 100 \
---n_print_steps 500 \
---n_val_steps 8531 \
+--n_epochs 50 \
+--n_print_steps 100 \
+--n_val_steps 2000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -33,7 +31,7 @@ python \
 --trm_layers 1 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
---libri_w2v2_weight 1.0 \
+--libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
