5393ee55dee3a926ca4f1371bc56e6bbc0c30599
khazarkhorrami <khazar.khorrami@gmail.com>
2023-09-17 15:44:41 +0300

    light to heavy test

--- scripts/ssl-j.sh
+++ scripts/ssl-j.sh
@@ -18,8 +18,8 @@ python \
 --libri_fn_root ${libri_fn_root} \
 --trained_weights_dir ${twd} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 4 \
---val_batch_size 4 \
+--batch_size 8 \
+--val_batch_size 8 \
 --val_cross_batch_size 16 \
 --n_epochs 30 \
 --n_print_steps 50 \
