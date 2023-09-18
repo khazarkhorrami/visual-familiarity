58746f8cbc79c82476609960e0fcdfb60167dd67
Khazar Khorrami <khazar.khorrami@gmail.com>
2023-09-18 11:07:23 +0300

    testing resume in j

--- scripts/ssl-j.sh
+++ scripts/ssl-j.sh
@@ -16,7 +16,7 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---trained_weights_dir ${twd} \
+--resume \
 --load_pretrained_vit ${pretrained_root} \
 --batch_size 8 \
 --val_batch_size 8 \
