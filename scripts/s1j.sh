7b3c441e094111a14a3fd501040fced4cb46658e
khazarkhorrami <khazar.khorrami@gmail.com>
2023-09-29 14:25:33 +0300

    ready to run on puhti

--- scripts/s1j.sh
+++ scripts/s1j.sh
@@ -20,11 +20,11 @@ python \
 --libri_fn_root ${libri_fn_root} \
 --load_pretrained_vit ${pretrained_root} \
 --batch_size 4 \
---val_batch_size 4 \
+--val_batch_size 16 \
 --val_cross_batch_size 100 \
 --n_epochs 100 \
 --n_print_steps 20 \
---n_val_steps 200 \
+--n_val_steps 2000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
