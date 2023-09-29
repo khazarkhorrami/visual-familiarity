41186d957f4f7ea1c58bdf133c7a5eb02f8c27df
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-09-29 14:49:33 +0300

    ready to run on puhti

--- scripts/s0.sh
+++ scripts/s0.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-source activate fastvgs
+export PATH="/projappl/project_2001315/khazar/con/vf/bin:$PATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../expS1/"
+exp_dir="../../expS0/"
 libri_fn_root="../../../../datavf/ssl6M_root/"
 pretrained_root="../../../../hubertAndDINO"
 twd="../../twd/"
@@ -12,18 +12,18 @@ twd="../../twd/"
 python \
 ../run_spokencoco.py \
 --image_type "normal" \
---subset "subset1" \
+--subset "subset0A" \
 --data_root ${data_root} \
 --trained_weights_dir ${twd} \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 4 \
---val_batch_size 16 \
+--batch_size 64 \
+--val_batch_size 64 \
 --val_cross_batch_size 100 \
---n_epochs 100 \
---n_print_steps 20 \
+--n_epochs 200 \
+--n_print_steps 40 \
 --n_val_steps 2000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
