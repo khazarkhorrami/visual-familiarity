57196b78a2327c2b527d724ad9b91d9491aeeb1c
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-10-02 15:12:53 +0300

    fixing ssl problems

--- scripts/sslh.sh
+++ scripts/sslh.sh
@@ -16,6 +16,7 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
+--trained_weights_dir ${twd} \
 --load_pretrained_vit ${pretrained_root} \
 --batch_size 180 \
 --val_batch_size 180 \
