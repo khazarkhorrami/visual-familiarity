626a5f9b4172fdfb08a1b9481464af83389ad4b1
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-02 14:27:34 +0300

    ssl bs

--- scripts/sslj.sh
+++ scripts/sslj.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../exph/"
+exp_dir="../../exphh/"
 twd="../../twd/"
 libri_fn_root="../../../../datavf/ssl6M_root/"
 pretrained_root="../../../../hubertAndDINO"
@@ -22,7 +22,7 @@ python \
 --val_cross_batch_size 100 \
 --n_epochs 50 \
 --n_print_steps 100 \
---n_val_steps 4265 \
+--n_val_steps 1000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
