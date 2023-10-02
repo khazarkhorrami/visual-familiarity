04f139923bf031bab9e15d34d89e7a302e7928e4
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-02 12:00:24 +0300

    testing 100 epoch

--- scripts/s1.sh
+++ scripts/s1.sh
@@ -22,9 +22,9 @@ python \
 --batch_size 64 \
 --val_batch_size 64 \
 --val_cross_batch_size 100 \
---n_epochs 200 \
+--n_epochs 100 \
 --n_print_steps 20 \
---n_val_steps 1000 \
+--n_val_steps 100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
