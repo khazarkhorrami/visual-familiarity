7f8b5c73b95553194e152b429f199bcfaf27a2ca
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-10-03 12:46:08 +0300

    testing simultaneous training

--- scripts/s1j.sh
+++ scripts/s1j.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
-export PATH="/projappl/project_2001315/khazar/con/vf/bin:$PATH"
+source activate fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../exphh/"
-twd="../../twd/"
-libri_fn_root="../../../../datavf/ssl6M_root/"
+exp_dir="../../expS1/"
+libri_fn_root="../../../../datavf/ssl_sub1_root/"
 pretrained_root="../../../../hubertAndDINO"
+twd="../../twd/"
 
 python \
 ../run_spokencoco.py \
---ssl \
+--image_type "normal" \
+--subset "subset1" \
 --data_root ${data_root} \
+--trained_weights_dir ${twd} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---trained_weights_dir ${twd} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 160 \
---val_batch_size 160 \
+--batch_size 4 \
+--val_batch_size 4 \
 --val_cross_batch_size 100 \
---n_epochs 70 \
---n_print_steps 100 \
---n_val_steps 3412 \
+--n_epochs 10 \
+--n_print_steps 20 \
+--n_val_steps 100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -30,12 +31,14 @@ python \
 --vit_checkpoint_key 'teacher' \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 1 \
+--trm_layers 6 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
---layer_use 7 \
+--encoder_layers 6 \
+--encoder_attention_heads 8 \
+--layer_use 4 \
 
