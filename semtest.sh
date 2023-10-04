35b29fcd7df725414e68911aa31c8ca8f0635bf6
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-04 15:56:14 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -7,21 +7,21 @@ MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/prefb"
 
 
 MNAME="expS1"
-SNAME="S1_aL_vO"
+SNAME="S1_aL_vM"
 AF="COCO"
-VF="images/original"
+VF="images/masked"
 python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 MNAME="expS2"
-SNAME="S2_aL_vO"
+SNAME="S2_aL_vM"
 AF="COCO"
-VF="images/original"
+VF="images/masked"
 python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 MNAME="expS3"
-SNAME="S3_aL_vO"
+SNAME="S3_aL_vM"
 AF="COCO"
-VF="images/original"
+VF="images/masked"
 python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 
