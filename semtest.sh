9e85eea00a5d681d0285e4cf284db964213dc16c
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-10-05 10:57:09 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -6,9 +6,9 @@ ROOT="/worktmp/khorrami/current"
 MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/pre6M"
 
 MNAME="expS3"
-SNAME="S3_aL_vM"
+SNAME="S3_aL_vO"
 AF="COCO"
-VF="images/masked"
+VF="images/original"
 python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 
