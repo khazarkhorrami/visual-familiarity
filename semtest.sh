a889f2491b2d4225dda75fb344dccf7b2c2641b3
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-05 10:50:28 +0300

    semtest 6m

--- semtest.sh
+++ semtest.sh
@@ -3,20 +3,7 @@
 source activate fastvgs
 
 ROOT="/worktmp/khorrami/current"
-MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/prefb"
-
-
-MNAME="expS1"
-SNAME="S1_aL_vM"
-AF="COCO"
-VF="images/masked"
-python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
-
-MNAME="expS2"
-SNAME="S2_aL_vM"
-AF="COCO"
-VF="images/masked"
-python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/pre6M"
 
 MNAME="expS3"
 SNAME="S3_aL_vM"
