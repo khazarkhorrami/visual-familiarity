8e83528b156eb7774b29d1785288c588af2f1941
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-10-06 11:53:21 +0300

    abx

--- abx.sh
+++ abx.sh
@@ -10,7 +10,17 @@ MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exphh"
 
 M="best_bundle.pth"
 
-for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+OUTNAME="L0"
+OUTFILE=$OUTFOLDER/$OUTNAME
+source activate fastvgs
+python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer 0 --mytwd $MFOLDER/$M
+conda activate zerospeech2021
+mkdir $OUTFILE
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+rm -r $EMBDFOLDER/$TYPE
+
+
+for LAYERNAME in 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
