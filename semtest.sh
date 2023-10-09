ff2e47f63833df0d9da678f744ed4b397414243e
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-09 16:03:25 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -3,12 +3,12 @@
 source activate fastvgs
 
 ROOT="/worktmp/khorrami/current"
-MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/pre6M"
+MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/exp100"
 
 MNAME="expS3"
 SNAME="S3_aL_vO"
 AF="COCO"
 VF="images/original"
-python semantic_eval_DINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 
