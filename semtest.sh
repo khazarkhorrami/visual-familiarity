e509060f3fdc8e43b4b73673f87b86542d280e3f
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-09 21:56:26 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -5,10 +5,20 @@ source activate fastvgs
 ROOT="/worktmp/khorrami/current"
 MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/exp100"
 
-MNAME="expS3"
-SNAME="S3_aL_vO"
+MNAME="expS0"
+SNAME="S0_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
+MNAME="expS0"
+SNAME="S0_aL_vM"
+AF="COCO"
+VF="images/masked"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
+MNAME="expS0"
+SNAME="S0_aL_vB"
+AF="COCO"
+VF="images/blurred"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
