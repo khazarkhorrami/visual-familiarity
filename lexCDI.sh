32c2cf82e5e9e43696cece4871e771f2ed40f20c
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-13 14:21:19 +0300

    new tests

--- lexCDI.sh
+++ lexCDI.sh
@@ -3,11 +3,11 @@ DATAFOLDER='/worktmp/khorrami/current/lextest/data/CDI/'
 EMBDFOLDER='/worktmp/khorrami/current/lextest/embedds/'
 source activate fastvgs
 
-NAME="exphh"
-OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfls/CDI"/$NAME
+NAME="expS0"
+OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/CDI"/$NAME
 mkdir $OUTFOLDER
-MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfls/exp70"/$NAME
-M="best_bundle.pth"
+MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
+M="bundle.pth"
 for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
@@ -19,4 +19,50 @@ do
     rm -r $EMBDFOLDER
 done
 
+NAME="expS1"
+OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/CDI"/$NAME
+mkdir $OUTFOLDER
+MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
+M="bundle.pth"
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    mkdir $OUTFILE
+    cd /worktmp/khorrami/current/lextest/CDI_lextest
+    sh CDI_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
+    rm -r $EMBDFOLDER
+done
 
+NAME="expS2"
+OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/CDI"/$NAME
+mkdir $OUTFOLDER
+MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
+M="bundle.pth"
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    mkdir $OUTFILE
+    cd /worktmp/khorrami/current/lextest/CDI_lextest
+    sh CDI_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
+    rm -r $EMBDFOLDER
+done
+
+NAME="expS3"
+OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/CDI"/$NAME
+mkdir $OUTFOLDER
+MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
+M="bundle.pth"
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    mkdir $OUTFILE
+    cd /worktmp/khorrami/current/lextest/CDI_lextest
+    sh CDI_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
+    rm -r $EMBDFOLDER
+done
--- semtest.sh
+++ semtest.sh
@@ -3,22 +3,79 @@
 source activate fastvgs
 
 ROOT="/worktmp/khorrami/current"
-MROOT="/worktmp/khorrami/current/FaST/experiments/vfsubsets/exp100"
+GNAME="vfsubsets"
+MROOT="/worktmp/khorrami/current/FaST/experiments/$GNAME/exp6M"
+mkdir $ROOT/"semtest"/"Smatrix"/$GNAME
+
 
 MNAME="expS0"
-SNAME="S0_aL_vO"
+
+SNAME=$GNAME/"S0_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-MNAME="expS0"
-SNAME="S0_aL_vM"
+SNAME=$GNAME/"S0_aL_vM"
 AF="COCO"
 VF="images/masked"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-MNAME="expS0"
-SNAME="S0_aL_vB"
+SNAME=$GNAME/"S0_aL_vB"
 AF="COCO"
 VF="images/blurred"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+
+
+MNAME="expS1"
+
+SNAME=$GNAME/"S1_aL_vO"
+AF="COCO"
+VF="images/original"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S1_aL_vM"
+AF="COCO"
+VF="images/masked"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S1_aL_vB"
+AF="COCO"
+VF="images/blurred"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+MNAME="expS2"
+
+SNAME=$GNAME/"S2_aL_vO"
+AF="COCO"
+VF="images/original"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S2_aL_vM"
+AF="COCO"
+VF="images/masked"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S2_aL_vB"
+AF="COCO"
+VF="images/blurred"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+
+MNAME="expS3"
+
+SNAME=$GNAME/"S3_aL_vO"
+AF="COCO"
+VF="images/original"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S3_aL_vM"
+AF="COCO"
+VF="images/masked"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
+SNAME=$GNAME/"S3_aL_vB"
+AF="COCO"
+VF="images/blurred"
+python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
+
