36fcc4ee726a99c3e951ce3e01bca3bebe82aaf9
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-14 23:41:59 +0300

    abx ls

---
+++ abxls.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+DATAFOLDER='/worktmp/khorrami/current/ZeroSpeech/data/phonetic/'
+EMBDFOLDER='/worktmp/khorrami/current/ZeroSpeech/submission'
+TYPE='phonetic/'
+NAME="vfls"
+
+OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/WC/vfls/expls"
+mkdir $OUTDIR
+MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfls/expls"
+
+MODEL="E10"
+M="${MODEL}_bundle.pth"
+OUTFOLDER=$OUTDIR/$MODEL
+mkdir $OUTFOLDER
+
+OUTNAME="L0"
+OUTFILE=$OUTFOLDER/$OUTNAME
+source activate fastvgs
+python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer 0 --mytwd $MFOLDER/$M
+conda activate zerospeech2021
+mkdir $OUTFILE
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu 
+rm -r $EMBDFOLDER/$TYPE
+
+for LAYERNAME in 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r $EMBDFOLDER/$TYPE
+done
+
+MODEL="E20"
+M="${MODEL}_bundle.pth"
+OUTFOLDER=$OUTDIR/$MODEL
+mkdir $OUTFOLDER
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r $EMBDFOLDER/$TYPE
+done
+
+MODEL="E40"
+M="${MODEL}_bundle.pth"
+OUTFOLDER=$OUTDIR/$MODEL
+mkdir $OUTFOLDER
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r $EMBDFOLDER/$TYPE
+done
+
+MODEL="E60"
+M="${MODEL}_bundle.pth"
+OUTFOLDER=$OUTDIR/$MODEL
+mkdir $OUTFOLDER
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r $EMBDFOLDER/$TYPE
+done
