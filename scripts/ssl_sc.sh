08204014904de8df6f2963eb6df1f511cddb521d
Khazar Khorrami <khazar.khorrami@gmail.com>
2023-10-15 09:49:17 +0300

    testing SSL

--- scripts/ssl_sc.sh
+++ scripts/ssl_sc.sh
@@ -4,9 +4,9 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../explsl/"
+exp_dir="../../exphh/"
 twd="../../twd/"
-libri_fn_root="../../../../datavf/libri_fn_root/"
+libri_fn_root="../../../../datavf/ssl6M_root/"
 pretrained_root="../../../../hubertAndDINO"
 
 python \
@@ -15,13 +15,14 @@ python \
 --data_root ${data_root} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
+--resume \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 180 \
---val_batch_size 180 \
+--batch_size 160 \
+--val_batch_size 160 \
 --val_cross_batch_size 100 \
 --n_epochs 50 \
 --n_print_steps 100 \
---n_val_steps 1800 \
+--n_val_steps 3412 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -36,7 +37,5 @@ python \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
---encoder_layers 8 \
---encoder_attention_heads 8 \
 --layer_use 7 \
 
