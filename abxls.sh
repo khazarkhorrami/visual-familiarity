893d6e7a390b82d53f53143ee3c8ca45c5f05782
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-10-16 15:49:53 +0300

    abx ls l

--- abxls.sh
+++ abxls.sh
@@ -8,7 +8,7 @@ OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/WC/vfls/explsl"
 mkdir $OUTDIR
 MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfls/explsl"
 
-MODEL="E10"
+MODEL="E40"
 M="${MODEL}_bundle.pth"
 OUTFOLDER=$OUTDIR/$MODEL
 mkdir $OUTFOLDER
@@ -22,7 +22,23 @@ mkdir $OUTFILE
 zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu 
 rm -r $EMBDFOLDER/$TYPE
 
-for LAYERNAME in 1 2 3 4 5
+for LAYERNAME in 1 2 3
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r $EMBDFOLDER/$TYPE
+done
+
+MODEL="E30"
+M="${MODEL}_bundle.pth"
+OUTFOLDER=$OUTDIR/$MODEL
+mkdir $OUTFOLDER
+for LAYERNAME in 0 1 2 3
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -38,23 +54,7 @@ MODEL="E20"
 M="${MODEL}_bundle.pth"
 OUTFOLDER=$OUTDIR/$MODEL
 mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5
-do
-    OUTNAME="L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r $EMBDFOLDER/$TYPE
-done
-
-MODEL="E40"
-M="${MODEL}_bundle.pth"
-OUTFOLDER=$OUTDIR/$MODEL
-mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5
+for LAYERNAME in 0 1 2 3
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -70,7 +70,7 @@ MODEL="E50"
 M="${MODEL}_bundle.pth"
 OUTFOLDER=$OUTDIR/$MODEL
 mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+for LAYERNAME in 0 1 2 3
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
