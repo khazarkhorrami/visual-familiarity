1a8d16aef40fde4438e78974451e61d639c82927
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-19 11:14:23 +0300

    testing ls

--- scripts/ssl_ls.sh
+++ scripts/ssl_ls.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../explsl/"
+exp_dir="../../expls10/"
 twd="../../twd/"
 libri_fn_root="../../../../datavf/libri_fn_root/"
 pretrained_root="../../../../hubertAndDINO"
@@ -16,9 +16,9 @@ python \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 16 \
---val_batch_size 16 \
---val_cross_batch_size 16 \
+--batch_size 22 \
+--val_batch_size 22 \
+--val_cross_batch_size 22 \
 --n_epochs 50 \
 --n_print_steps 100 \
 --n_val_steps 1800 \
@@ -36,7 +36,7 @@ python \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
 --trim_mask \
---encoder_layers 12 \
+--encoder_layers 10 \
 --encoder_attention_heads 8 \
---layer_use 11 \
+--layer_use 9 \
 
