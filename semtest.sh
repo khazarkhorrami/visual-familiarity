d4b46fb2a21c114beb7a9db9ab20d0aa9a1d0bf0
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-20 17:49:16 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -2,25 +2,27 @@
 
 source activate fastvgs
 
+M="RCNN"
+G="expR"
 ROOT="/worktmp/khorrami/current"
-GNAME="vfsubsets"
-MROOT="/worktmp/khorrami/current/FaST/experiments/$GNAME/exp6M"
-mkdir $ROOT/"semtest"/"Smatrix"/$GNAME
+MROOT="/worktmp/khorrami/current/FaST/experiments"/$M/$G
+
+mkdir $ROOT/"semtest/S"/$M/$G
 
 
 MNAME="expS0"
 
-SNAME=$GNAME/"S0_aL_vO"
+SNAME=$M/$G/"S0_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S0_aL_vM"
+SNAME=$M/$G/"S0_aL_vM"
 AF="COCO"
 VF="images/masked"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S0_aL_vB"
+SNAME=$M/$G/"S0_aL_vB"
 AF="COCO"
 VF="images/blurred"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
@@ -29,34 +31,34 @@ python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $
 
 MNAME="expS1"
 
-SNAME=$GNAME/"S1_aL_vO"
+SNAME=$M/$G/"S1_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S1_aL_vM"
+SNAME=$M/$G/"S1_aL_vM"
 AF="COCO"
 VF="images/masked"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S1_aL_vB"
+SNAME=$M/$G/"S1_aL_vB"
 AF="COCO"
 VF="images/blurred"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
 MNAME="expS2"
 
-SNAME=$GNAME/"S2_aL_vO"
+SNAME=$M/$G/"S2_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S2_aL_vM"
+SNAME=$M/$G/"S2_aL_vM"
 AF="COCO"
 VF="images/masked"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S2_aL_vB"
+SNAME=$M/$G/"S2_aL_vB"
 AF="COCO"
 VF="images/blurred"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
@@ -64,17 +66,17 @@ python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $
 
 MNAME="expS3"
 
-SNAME=$GNAME/"S3_aL_vO"
+SNAME=$M/$G/"S3_aL_vO"
 AF="COCO"
 VF="images/original"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S3_aL_vM"
+SNAME=$M/$G/"S3_aL_vM"
 AF="COCO"
 VF="images/masked"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
 
-SNAME=$GNAME/"S3_aL_vB"
+SNAME=$M/$G/"S3_aL_vB"
 AF="COCO"
 VF="images/blurred"
 python semDINO.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF --vfiles $VF
