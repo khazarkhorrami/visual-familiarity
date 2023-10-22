9fd569d11ce6e2de462fc49b27736a8379e9654b
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-10-22 15:35:03 +0300

    getting results

--- lexCOCO.sh
+++ lexCOCO.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 DATAFOLDER='/worktmp/khorrami/current/lextest/data/COCO/'
 EMBDFOLDER='/worktmp/khorrami/current/lextest/embedds/'
+OUTDIR='/worktmp/khorrami/current/lextest/output/COCO'
 source activate fastvgs
 
+M="RCNN"
+G="expR"
+OUTPATH=$OUTDIR/$M/$G
+mkdir $OUTPATH
+
+
 NAME="expS0"
-OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/COCO"/$NAME
+OUTFOLDER=$OUTPATH/$NAME
 mkdir $OUTFOLDER
-MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
-M="bundle.pth"
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$M/$G/$NAME
+B="bundle.pth"
 for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
-    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$B
     mkdir $OUTFILE
     cd /worktmp/khorrami/current/lextest/COCO_lextest
     sh COCO_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
@@ -21,15 +28,15 @@ done
 
 
 NAME="expS1"
-OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/COCO"/$NAME
+OUTFOLDER=$OUTPATH/$NAME
 mkdir $OUTFOLDER
-MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
-M="bundle.pth"
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$M/$G/$NAME
+B="bundle.pth"
 for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
-    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$B
     mkdir $OUTFILE
     cd /worktmp/khorrami/current/lextest/COCO_lextest
     sh COCO_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
@@ -37,15 +44,15 @@ do
 done
 
 NAME="expS2"
-OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/COCO"/$NAME
+OUTFOLDER=$OUTPATH/$NAME
 mkdir $OUTFOLDER
-MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
-M="bundle.pth"
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$M/$G/$NAME
+B="bundle.pth"
 for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
-    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$B
     mkdir $OUTFILE
     cd /worktmp/khorrami/current/lextest/COCO_lextest
     sh COCO_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
@@ -53,15 +60,15 @@ do
 done
 
 NAME="expS3"
-OUTFOLDER="/worktmp/khorrami/current/lextest/output/vfplus/COCO"/$NAME
+OUTFOLDER=$OUTPATH/$NAME
 mkdir $OUTFOLDER
-MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfplus/exp6M"/$NAME
-M="bundle.pth"
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$M/$G/$NAME
+B="bundle.pth"
 for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
-    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$B
     mkdir $OUTFILE
     cd /worktmp/khorrami/current/lextest/COCO_lextest
     sh COCO_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
