5463dfa34a58048ae6e4745ac3d06973c9b2f6c4
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-10-23 18:48:08 +0300

    new tests for loading weights

--- scripts/sslj.sh
+++ scripts/sslj.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../exph/"
+exp_dir="../../expnew/"
 twd="../../twd/"
 libri_fn_root="../../../../datavf/ssl6M_root/"
 pretrained_root="../../../../hubertAndDINO"
@@ -17,12 +17,13 @@ python \
 --libri_fn_root ${libri_fn_root} \
 --trained_weights_dir ${twd} \
 --load_pretrained_vit ${pretrained_root} \
---batch_size 12 \
---val_batch_size 16 \
+--libri_max_seq_len 10 \
+--batch_size 32 \
+--val_batch_size 32 \
 --val_cross_batch_size 100 \
---n_epochs 50 \
+--n_epochs 100 \
 --n_print_steps 100 \
---n_val_steps 1000 \
+--n_val_steps 3412 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --vit_arch 'vitsmall' \
@@ -35,7 +36,9 @@ python \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
---feature_grad_mult 1.0 \
+--feature_grad_mult 0.1 \
 --trim_mask \
---layer_use 7 \
+--encoder_layers 12 \
+--encoder_attention_heads 8 \
+--layer_use 11 \
 
