3be2ff3bb2885b5d53e9ae895a064fd317c3ab51
Khazar Khorrami <khazar.khorrami@gmail.com>
2023-10-25 18:16:37 +0300

    plot lex

--- lextest.py
+++ lextest.py
@@ -1,11 +1,6 @@
 root = "/worktmp2/hxkhkh/current/lextest/"
-dtype = 'COCO'
-mtype = 'vfplus'
-
-if mtype == 'vfplus':
-    title = 'RCNN-' + dtype
-elif mtype == 'vfsubsets':
-    title = 'DINO-' + dtype 
+dtype = 'CDI'
+mtype = 'DINO'
 
 
 #%%
@@ -20,17 +15,29 @@ def read_score (path):
     score = float(a[16:-1])
     return score
 
-def read_all_layers (model_name, tn):
+def read_all_layers (path, model_name, tn):
     scores = []
     layer_names = ['L1','L2','L3','L4','L5','L6','L7','L8', 'L9', 'L10', 'L11']
     for layer_name in layer_names:
         name = layer_name
-        path = os.path.join(path_input, model_name , name , tn)
-        s = read_score (path)
+        path_full = os.path.join(path , model_name,  name , tn)
+        s = read_score (path_full)
         scores.append(s)
     bs = np.sort(scores)[::-1][0]
     bl = np.argsort(scores)[::-1][0]
     return scores, bs, bl
+
+def get_best_score(path, models, tn):
+    dict_scores = {}
+    dict_bs = {}
+    dict_bl = {}
+    
+    for model in models:
+        scores, bs, bl = read_all_layers (path, model, tn)
+        dict_scores[model] = scores
+        dict_bs[model] = bs
+        dict_bl[model] = bl
+    return dict_scores, dict_bs, dict_bl
 
 def plotbar_single (names, results , title, cl,logmel):
     barWidth = 0.25
@@ -42,21 +49,57 @@ def plotbar_single (names, results , title, cl,logmel):
 
     ychance = np.ones(len(br1))*cl
     ylogmel = np.ones(len(br1))*logmel
-    plt.plot(br1,ychance, color ='orange', label='chance level', linewidth=1.5)
-    plt.plot(br1,ylogmel, color ='green', label='log-Mel features', linewidth=1.5)
+    plt.plot(br1,ychance, color ='red', label='chance level', linewidth=1.5)
+    plt.plot(br1,ylogmel, color ='black', label='log-Mel features', linewidth=1.5)
     plt.bar(br1, results, color ='b', width = barWidth,
             edgecolor ='grey')
-    plt.ylabel('lexical test' + '\n', fontweight ='bold', fontsize = 24)
-    plt.xticks([r for r in range(n)], names, fontweight ='bold',fontsize = 20)
+    plt.ylabel('lexical test' + '\n', fontweight ='bold', fontsize = 28)
+    plt.xticks([r for r in range(n)], names, fontweight ='bold',fontsize = 26)
+    plt.yticks(fontsize = 26)
     plt.ylim(0,1) 
-    plt.legend(fontsize = 24)
+    plt.legend(fontsize = 26, framealpha=0.1)
     plt.grid()
     plt.title(title + '\n', fontweight ='bold', fontsize = 26)
     savepath = os.path.join(root, "results/" )
-    plt.savefig(savepath + title + '.png' ,  format = 'png' )
+    plt.savefig(savepath + 'lex' + title + '.png' ,  format = 'png' )
+    plt.show()
+    
+def plotbar_multi_all (names, results , title, cl, logmel):
+    barWidth = 0.25
+    fig = plt.subplots(figsize =(12, 12))  
+    
+    n = len(results[0])
+    
+    br1 = np.arange(n)
+    br2 = [x + barWidth for x in br1]
+    br3 = [x + barWidth for x in br2]
+    x = [-barWidth]
+    x.extend(list(br1))
+    x.extend([float (list(br1)[-1] + 3*barWidth )])
+   
+    ychance = np.ones(len(br1))*cl
+    ylogmel = np.ones(len(br1))*logmel
+    plt.plot(br1,ychance, color ='red', label='chance level', linewidth=1.5)
+    plt.plot(br1,ylogmel, color ='black', label='log-Mel features', linewidth=1.5)
+    
+    plt.bar(br1, results[0], color ='b', width = barWidth,
+            edgecolor ='grey', label =title[0])
+    plt.bar(br2, results[1], color ='g', width = barWidth,
+            edgecolor ='grey', label =title[1])
+    plt.bar(br3, results[2], color ='grey', width = barWidth,
+            edgecolor ='grey', label =title[2])
+    plt.title("comparison between all pretrained versions" + ' (' + dtype + ')' , fontweight ='bold', fontsize = 28)
+    plt.ylabel('lexical test' + '\n', fontweight ='bold', fontsize = 28)
+    plt.xticks([r + barWidth for r in range(n)], names, fontweight ='bold',fontsize = 26)
+    plt.yticks(fontsize = 26)
+    #plt.ylim(0,1) 
+    plt.legend(fontsize = 24)
+    plt.grid()
+    savepath = os.path.join(root, "results/" )
+    plt.savefig(savepath + 'lexall' + '_' + dtype + '_' + mtype + '.png' ,  format = 'png' )
     plt.show()
 #%%    
-path_input = os.path.join(root, 'output/', mtype, dtype )
+
 
 if dtype=='COCO':
     tn = 'lextest_overall_score.txt'
@@ -67,31 +110,78 @@ else:
     cl = 1/89
     logmel = 0.17
     
-if mtype=='vfsubsets' or mtype=='vfplus':
-    models = ['exphh','expS1', 'expS0', 'expS2', 'expS3']
-    names = ["Speech only\n(6 months)", "subset 1\n(2 months)", "subset 0\n(uniform)", "subset 2\n(4 months)","subset 3\n(6 months)"]
-elif mtype=='vfls':
+if mtype=='RCNN' or mtype=='DINO':
+    models = ['expS1', 'expS0', 'expS2', 'expS3']
+    names = [ "2\n months", "4\n months\n (uniform)", "4\n months","6\n months"]
+elif mtype=='SSL':
     models = ['exphh']
     names = ["SSL 6M"]
     
-#%%
+#%% FB, single
 
-dict_scores = {}
-dict_bs = {}
-dict_bl = {}
-
-for model in models:
-    scores, bs, bl = read_all_layers (model, tn)
-    dict_scores[model] = scores
-    dict_bs[model] = bs
-    dict_bl[model] = bl
-
-#%%
-x = []
-results = []
+exp = 'expFB'
+expname = "FB"
+title = mtype + '-' + dtype + '-' + expname
+path_input = os.path.join(root, 'output/', dtype, mtype, exp )
+dict_scores, dict_bs, dict_bl = get_best_score(path_input, models, tn)
+x_FB = []
+results_FB = []
 for key, value in dict_bs.items():
-    x.append(key)
-    results.append(value)
+    x_FB.append(key)
+    results_FB.append(value)
+
+plotbar_single (names, results_FB, title, cl, logmel)    
+# dict_scores, dict_bs, dict_bl = get_best_score(models, tn)
+# # plotting
+# x = []
+# results = []
+# for key, value in dict_bs.items():
+#     x.append(key)
+#     results.append(value)
 
 
-plotbar_single (names, results, title, cl, logmel)
+# plotbar_single (names, results, title, cl, logmel)
+
+#%% all pretrained models
+kh
+results = []
+title = []
+
+exp = 'expFB'
+expname = "FB"
+title.append('Pre' + expname)
+path_input = os.path.join(root, 'output/', dtype, mtype, exp )
+dict_scores, dict_bs, dict_bl = get_best_score(path_input, models, tn)
+x_FB = []
+results_FB = []
+for key, value in dict_bs.items():
+    x_FB.append(key)
+    results_FB.append(value)
+    
+exp = 'exp6M'
+expname = "6M"
+title.append('Pre' + expname)
+path_input = os.path.join(root, 'output/', dtype, mtype, exp )
+dict_scores, dict_bs, dict_bl = get_best_score(path_input, models, tn)
+x_6M = []
+results_6M = []
+for key, value in dict_bs.items():
+    x_6M.append(key)
+    results_6M.append(value)
+ 
+exp = 'expR'
+expname = "R"
+title.append('Pre' + expname)
+path_input = os.path.join(root, 'output/', dtype, mtype, exp )
+dict_scores, dict_bs, dict_bl = get_best_score(path_input,models, tn)
+x_R = []
+results_R = []
+for key, value in dict_bs.items():
+    x_R.append(key)
+    results_R.append(value)
+
+results.append(results_FB)
+results.append(results_6M) 
+results.append(results_R) 
+
+plotbar_multi_all (names, results , title,cl, logmel)
