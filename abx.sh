829cc98c94332485a6aa67214cadf0e2cf7bc8b8
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-10-25 11:29:59 +0300

    abx expnew

--- abx.sh
+++ abx.sh
@@ -2,13 +2,13 @@
 DATAFOLDER='/worktmp/khorrami/current/ZeroSpeech/data/phonetic/'
 EMBDFOLDER='/worktmp/khorrami/current/ZeroSpeech/submission'
 TYPE='phonetic/'
-NAME="vfsubsets"
-M="bundle.pth"
+NAME="vfls"
+M="E10_bundle.pth"
 OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/WC"/$NAME
 mkdir $OUTDIR
 
-MODEL='expS1'
-MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"expM6"/$MODEL
+MODEL='expnew'
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/$MODEL
 OUTFOLDER=$OUTDIR/$MODEL
 mkdir $OUTFOLDER
 
@@ -33,51 +33,3 @@ do
     rm -r $EMBDFOLDER/$TYPE
 done
 
-
-MODEL='expS0'
-MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp6M"/$MODEL
-OUTFOLDER=$OUTDIR/$MODEL
-mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
-do
-    OUTNAME="L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r $EMBDFOLDER/$TYPE
-done
-
-MODEL='expS2'
-MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp6M"/$MODEL
-OUTFOLDER=$OUTDIR/$MODEL
-mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
-do
-    OUTNAME="L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r $EMBDFOLDER/$TYPE
-done
-
-MODEL='expS3'
-MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp6M"/$MODEL
-OUTFOLDER=$OUTDIR/$MODEL
-mkdir $OUTFOLDER
-for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
-do
-    OUTNAME="L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r $EMBDFOLDER/$TYPE
-done
