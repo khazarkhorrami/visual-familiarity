0a8a026a49993a555ab52e1e04835977b994ddea
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-11-08 11:31:39 +0200

    abx 15

--- abx.sh
+++ abx.sh
@@ -3,18 +3,18 @@ DATAFOLDER='/worktmp/khorrami/current/ZeroSpeech/data/phonetic/'
 EMBDFOLDER='/worktmp/khorrami/current/ZeroSpeech/submission'
 TYPE='phonetic/'
 NAME="vfls"
-M="best_bundle.pth"
+M="E20_bundle.pth"
 OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/WC"/$NAME
 mkdir $OUTDIR
 
-MODEL='expnewl8'
+MODEL='expnewl15'
 MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/$MODEL
 
-OUTFOLDER=$OUTDIR/$MODEL/"best"
+OUTFOLDER=$OUTDIR/$MODEL/"E20"
 mkdir $OUTDIR/$MODEL
 mkdir $OUTFOLDER
 
-OUTNAME="L5"
+OUTNAME="L3"
 OUTFILE=$OUTFOLDER/$OUTNAME
 source activate fastvgs
 python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer 0 --mytwd $MFOLDER/$M
@@ -23,7 +23,7 @@ mkdir $OUTFILE
 zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu 
 rm -r $EMBDFOLDER/$TYPE
 
-for LAYERNAME in 1 6
+for LAYERNAME in 2 4 5
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
