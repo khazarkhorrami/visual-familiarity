6581b1bb2b137e66005e287991e22bdbf646bae1
khazarkhorrami <khazar.khorrami@gmail.com>
2023-11-27 17:08:18 +0200

    new scores

---
+++ lexCDIPre.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+DATAFOLDER='/worktmp/khorrami/current/lextest/data/CDI/'
+EMBDFOLDER='/worktmp/khorrami/current/lextest/embedds/'
+OUTDIR='/worktmp/khorrami/current/lextest/output/CDI'
+source activate fastvgs
+
+M="ssl"
+G="exp15"
+OUTFOLDER=$OUTDIR/$M/$G
+mkdir $OUTFOLDER
+
+MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$M/$G
+B="E90_bundle.pth"
+for LAYERNAME in 0 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    python /worktmp/khorrami/current/FaST/visual-familiarity/lexical.py --apath $DATAFOLDER --epath $EMBDFOLDER --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$B
+    mkdir $OUTFILE
+    cd /worktmp/khorrami/current/lextest/CDI_lextest
+    sh CDI_lextest.sh $DATAFOLDER $EMBDFOLDER 'single' 0 $OUTFILE
+    rm -r $EMBDFOLDER
+done
