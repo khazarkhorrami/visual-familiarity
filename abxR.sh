a207c8c2a595e6ee996b62a163d6b3d26345e67d
khazarkhorrami <khazar.khorrami@gmail.com>
2023-12-12 22:20:42 +0200

    abx across

--- abxR.sh
+++ abxR.sh
@@ -4,7 +4,7 @@ EMBDFOLDER='/worktmp/khorrami/current/ZeroSpeech/submission'
 TYPE='phonetic/'
 NAME="baseline"
 M="bundle.pth"
-OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/WC"/$NAME
+OUTDIR="/worktmp/khorrami/current/ZeroSpeech/output/AC"/$NAME
 mkdir $OUTDIR
 
 MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfrand/exprandom"
@@ -12,7 +12,7 @@ MFOLDER="/worktmp/khorrami/current/FaST/experiments/vfrand/exprandom"
 OUTFOLDER=$OUTDIR
 mkdir $OUTFOLDER
 
-OUTNAME="L4"
+OUTNAME="L1"
 OUTFILE=$OUTFOLDER/$OUTNAME
 source activate fastvgs
 python /worktmp/khorrami/current/FaST/visual-familiarity/abx.py --apath $DATAFOLDER --epath $EMBDFOLDER/$TYPE --mytarget_layer 0 --mytwd $MFOLDER/$M
@@ -21,7 +21,7 @@ mkdir $OUTFILE
 zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  $EMBDFOLDER -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu 
 rm -r $EMBDFOLDER/$TYPE
 
-for LAYERNAME in 5 6 7 
+for LAYERNAME in 2 3 4 5 6 7 
 do
     OUTNAME="L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
